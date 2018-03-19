70a0b41e1b8ee20293c085e4baeff42e54a23daa
Tomoyuki Sahara <sahara@caddr.net>
2018-03-19 22:41:48 +0900

    add mac-address-hash

--- mac-address.scm
+++ mac-address.scm
@@ -2,6 +2,7 @@
   (use gauche.sequence)
   (use gauche.uvector)
   (export mac-address-cmp
+	  mac-address-hash
 	  string->mac-address))
 
 (select-module mac-address)
@@ -9,6 +10,9 @@
 (define (mac-address-cmp a b)
   (and (= (u8vector-length a) (u8vector-length b))
        (every = (u8vector->list a) (u8vector->list b))))
+
+(define (mac-address-hash mac max)
+  (modulo (apply + (u8vector->list mac)) max))
 
 (define (string->mac-address str)
   (list->u8vector (map (cut string->number <> 16)
