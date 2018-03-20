3dea5f9cef2148d673744c52aeb90a0a33734552
Tomoyuki Sahara <sahara@caddr.net>
2018-03-20 22:04:45 +0900

    wip

---
+++ tcpsim.scm
@@ -0,0 +1,20 @@
+(define-class <tcp> ()
+  ())
+
+(define (tcp-open-to peer)
+  )
+
+(define-class <tcp-segment> ()
+  ())
+
+(define-method (on-receive segment)
+
+  )
+
+(define (timer-event-add sec thunk)
+  (event-)
+
+(let ((a (make <tcp>))
+      (b (make <tcp>)))
+  (tcp-open-to b)
+  (event-loop))
