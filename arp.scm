0d7277b6237605d39226839e44962215ec449806
Tomoyuki Sahara <sahara@caddr.net>
2018-03-22 23:27:32 +0900

    arp-table-delete!

--- arp.scm
+++ arp.scm
@@ -1,33 +1,36 @@
 (define-module arp
+  (use mac-address)
   (use gauche.uvector)
+  (use srfi-69)
   (export make-arp-table
-	  arp-table-add
-	  arp-table-find-by-ipv4-address
+	  arp-table-add!
+	  arp-table-delete!
+	  arp-table-get
 	  ))
 
 (select-module arp)
 
 (define-class <arp-table> ()
-  ((alist :init-value '())))
+  ((ht :init-form (make-hash-table mac-address-cmp mac-address-hash))))
 
 (define (make-arp-table)
   (make <arp-table>))
 
-(define (arp-table-find-by-ipv4-address arptab ipaddr)
-  (let1 p (assoc ipaddr (slot-ref arptab 'alist))
-    (if p
-	(cdr p)
-	#f)))
+(define (arp-table-get arptab ip)
+  (hash-table-ref/default (ref arptab 'ht) ip #f))
+
+(define (arp-table-add! arptab ip mac)
+  (hash-table-set! (ref arptab 'ht) ip mac))
+
+(define (arp-table-delete! arptab ip)
+  (hash-table-delete! (ref arptab 'ht) ip))
+
 
 (define (arp-table-find-by-mac-address arptab mac)
   (let1 p (rassoc mac (slot-ref arptab 'alist))
     (if p
 	(cdr p)
 	#f)))
-
-(define (arp-table-add arptab ipaddr macaddr)
-  (slot-set! arptab 'alist
-	     (acons ipaddr macaddr (slot-ref arptab 'alist))))
 
 (define (arp-table->list arptab)
   (slot-ref arptab 'alist))
--- test/test_arp.scm
+++ test/test_arp.scm
@@ -1,11 +1,10 @@
 (use gauche.test)
 (use ipv4)
-(use macaddr)
+(use mac-address)
 
 (test-start "arp")
 (load "arp")
 (import arp)
-
 
 (test-module 'arp)
 
@@ -14,8 +13,13 @@
 (let ((t    (make-arp-table))
       (ip0  (string->ipv4-address "192.168.0.1"))
       (mac0 (string->mac-address  "02:01:02:03:04:05")))
-  (arp-table-add t ip0 mac0)
 
-  (test* "arp table add" mac0 (arp-table-find-by-ipv4-address t ip0)))
+  (arp-table-add! t ip0 mac0)
+  (test* "arp table add" mac0 (arp-table-get t ip0))
+
+  (arp-table-delete! t ip0)
+  (test* "arp table delete" #f (arp-table-get t ip0))
+
+  )
 
 (test-end)
